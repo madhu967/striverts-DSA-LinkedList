dd767afb143a813696f051e76dcbbe14ff08e793
madhu967 <156585768+madhu967@users.noreply.github.com>
2025-09-11 07:44:16 +0530

    Create Traversal.java

---
+++ Traversal.java
@@ -0,0 +1,39 @@
+package LinkedList;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data1,Node next1){
+        this.data=data1;
+        this.next=next1;
+    }
+    Node(int data1){
+        this.data=data1;
+        this.next=null;
+    }
+}
+
+public class traversal {
+    private static Node convertToLL(int[] arr){
+        Node head=new Node(arr[0]);
+        Node mover=head;
+        for(int i=1;i<arr.length;i++){
+           Node temp=new Node(arr[i]);
+           mover.next=temp;
+           mover=temp;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={12,4,5,6};
+        Node head=convertToLL(arr);
+        System.out.println(head);
+        Node temp=head;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
+}
