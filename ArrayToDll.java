abd75991561d08c0d672f14c14e4d82349ab6b91
madhu967 <156585768+madhu967@users.noreply.github.com>
2025-09-15 18:25:45 +0530

    Create ArrayToDll.java

---
+++ ArrayToDll.java
@@ -0,0 +1,46 @@
+package doublelinkedlist;
+
+class Node{
+    int data;
+    Node next;
+    Node back;
+
+    Node(int data1,Node next1,Node back1){
+        this.data=data1;
+        this.next=next1;
+        this.back=back1;
+    }
+
+    Node(int data1){
+        data=data1;
+        next=null;
+        back=null;
+    }
+}
+
+public class arraytodll {
+
+    private static Node convertToDll(int[] arr){
+        Node head=new Node(arr[0]);
+        Node prev=head;
+        for(int i=1;i<arr.length;i++){
+            Node temp=new Node(arr[i],null,prev);
+            prev.next=temp;
+            prev=temp;
+        }
+        return head;
+    }
+    private static void print(Node head){
+        while (head!=null) {
+            System.out.print(head.data+" ");
+            head=head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr={12,5,8,7};
+        Node head=convertToDll(arr);
+        System.out.println(head.data);
+        print(head);
+    }
+}
